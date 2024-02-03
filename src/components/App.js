0f447c0cd851ac5c9f663286043beda76e2edc60
Victor Muturi <muturivictor21@gmail.com>
2024-02-03 14:30:02 +0300

    Completeed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,16 @@
 import React from "react";
+import TaskList from "./TaskList";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
-
-import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+import { TASKS, CATEGORIES } from "../data";
 
 function App() {
   return (
-    <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+    <div>
+      <h2>Task List</h2>
+      <CategoryFilter categories={CATEGORIES} />
+      <TaskList tasks={TASKS} />
+      <NewTaskForm categories={CATEGORIES} />
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState(categories[0]); // Default to first category
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Use onTaskFormSubmit callback prop to add new task
+  };
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={text} onChange={handleTextChange} />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" value={category} onChange={handleCategoryChange}>
+          {categories.map((category) => (
+            <option key={category} value={category}>{category}</option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
